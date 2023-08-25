76fc866b2b2c1a0952e6d2474696440aab5e7764
yaqeen009 <wunzaligu19@gmail.com>
2023-08-25 00:32:15 +0000

    updates

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -1,32 +1,24 @@
-
 import ChatScreen from "./Chat Screen/ChatScreen";
 import ConvoList from "./ConvoList";
 
-
-
 const ChatWindow: React.FC = () => {
-    return (
-      <div className='ChatWindow'>
-          <div className= 'bg-primary flex flex-row'>
-              <div className="basis-1/5 my-5 mx-3">
-                {/* <h1 className="text-3xl text-secondary-300 font-bold  text-center">Chats</h1> */}
-                <ConvoList/>
-              </div>
-              <div className="basis-1/2">
-                <ChatScreen/>
-              </div>
-              {/* <div className="basis-1/4">
-              
-              </div> */}
-              
-          </div>
+  return (
+    <div className="ChatWindow">
+      <div className="bg-primary flex flex-row">
+        <div className="basis-1/5 my-5 mx-3">
+          {/* <h1 className="text-3xl text-secondary-300 font-bold  text-center">Chats</h1> */}
+          <ConvoList />
+        </div>
+        <div className="basis-1/2">
+          <ChatScreen />
+        </div>
+        <div className="basis-1/4"></div>
       </div>
+    </div>
+  );
+};
 
-    );
-  };
-  
-  export default ChatWindow;
-  
+export default ChatWindow;
 
-//  
-//                
+//
+//
---
+++ src/components/Recipient/Recipient.tsx
@@ -0,0 +1,11 @@
+
+
+const Recipient = () => {
+    return (
+        <div className="bg-black-100">
+            
+        </div>
+    );
+};
+
+export default Recipient;
